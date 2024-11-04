82ec41a334f6f55f77e93b6d7df17e0816f25569
Natalia Tarakci <nataliatarakci@gmail.com>
2024-11-04 18:19:26 +0000

    v4

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -9,4 +9,6 @@ public class MyClass {
     public String str4 = "This is a String created in the techpro branch.";
 
     public String str5 = "This is another String created in the techpro branch.";
+
+    public String str6 = "This is one more String.";
 }
